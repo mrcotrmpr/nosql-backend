1e68624301890c918c11d5f2fc7a28ca2a245a75
mrcotrmpr <marco1088@hotmail.nl>
2021-11-29 17:14:22 +0100

    set up generic voting structure

--- src/controllers/subcomment.controller.js
+++ src/controllers/subcomment.controller.js
@@ -46,46 +46,5 @@ module.exports = {
                 return res.status(200).send(subcomment);
             });
          })
-    },
-
-    async upvote(req, res, next){
-        await Subcomment.findOne({ _id: req.body.id })
-        .then((subcomment) => {    
-            if(!subcomment){
-                res.status(204).send({message: "Subcomment with id" + req.body.id + " was not found"});
-            };
-            if(subcomment.upvotes.includes(req.body.username)){
-                return res.status(405).send({message: "you can only upvote once"});
     }  
-            if(!subcomment.upvotes.includes(req.body.username)){
-                if(subcomment.downvotes.includes(req.body.username)){
-                    Subcomment.findByIdAndUpdate({_id: req.body.id}, {$pull: {downvotes: req.body.username}}, {upsert: true}, function(){})
-                }
-                Subcomment.findByIdAndUpdate({_id: req.body.id}, {$push: {upvotes: req.body.username}}, {upsert: true}, function(err, doc) {
-                    return res.status(200).send({message: "upvote added"});
-                });          
-            };
-        })
-    },
-
-    async downvote(req, res, next){
-        await Subcomment.findOne({ _id: req.body.id })
-        .then((subcomment) => {    
-            if(!subcomment){
-                res.status(204).send({message: "Subcomment with id" + req.body.id + " was not found"});
-            };
-            if(subcomment.downvotes.includes(req.body.username)){
-                return res.status(405).send({message: "you can only downvote once"});
-            }
-            if(!subcomment.downvotes.includes(req.body.username)){
-                if(subcomment.upvotes.includes(req.body.username)){
-                    Subcomment.findByIdAndUpdate({_id: req.body.id}, {$pull: {upvotes: req.body.username}}, {upsert: true}, function() {})
-                }
-                Subcomment.findByIdAndUpdate({_id: req.body.id}, {$push: {downvotes: req.body.username}}, {upsert: true}, function(err, doc) {
-                    return res.status(200).send({message: "downvote added"});
-                });
-            };
-        })
-    }
-  
 }
